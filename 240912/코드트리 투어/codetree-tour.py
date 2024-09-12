81f7522082ae4fcc46ddc2cbcc8af06ee1c9b617
bhdj0107 <43875772+bhdj0107@users.noreply.github.com>
2024-09-12 16:43:29 +0900

    Title: 코드트리 투어, Time: 3170ms, Memory: 90MB, Status: Passed - CodeTree

--- 240912/코드트리 투어/codetree-tour.py
+++ 240912/코드트리 투어/codetree-tour.py
@@ -1,10 +1,22 @@
 from collections import defaultdict
+import heapq
 import math
 # 명령의 수 Q, 도시 의 수 n, 간선의 수 m
 # v 도시와 u 도시는 w 길이의 간선으로 연결됨
 
 Q = int(input())
 
+
+class Package:
+    def __init__(self, id, revCost):
+        self.id = id
+        self.revCost = revCost
+        
+    def __lt__(self, other):
+        if self.revCost < other.revCost: return False
+        elif self.revCost > other.revCost: return True
+        else:
+            return self.id < other.id
 
 def daijkstra(connection, startPt):
     dist = [math.inf for _ in range(len(connection))]
@@ -29,20 +41,12 @@ def daijkstra(connection, startPt):
                 dist[i] = min(dist[i], minDist + connection[minPos][i])
     return dist
 
-def sellablePackage(package, dist):
-    ret = []
-    for package_id, (rev, dst) in package.items():
-        if rev - dist[dst] >= 0: ret.append((package_id, rev - dist[dst]))
-    ret.sort(key=lambda x: (-x[1], x[0]), reverse=True)
-    return ret
-
+heap = []
 package = {}
-prevCmd = -100
 
 for t in range(Q):
     query = list(map(int, input().split()))
     if query[0] == 100:
-        prevCmd = 100
         N, M = query[1:3]
         connection = [[math.inf for _ in range(N)] for _ in range(N)]
         for i in range(N): connection[i][i] = 0
@@ -53,27 +57,33 @@ for t in range(Q):
         dist = daijkstra(connection, 0)
         
     elif query[0] == 200:
-        prevCmd = 200
         package_id, rev, dst = query[1:]
         package[package_id] = (rev, dst)
+        revCost = rev - dist[dst]
+        pkg = Package(package_id, revCost)
+        heapq.heappush(heap, pkg)
 
     elif query[0] == 300:
-        prevCmd = 300
         package_id = query[1]
         if package.get(package_id):
             del package[package_id]
     
     elif query[0] == 400:
-        if prevCmd != 400:
-            sellable = sellablePackage(package, dist)
-        prevCmd = 400
-        if len(sellable):
-            pid = sellable.pop()[0]
-            print(pid)
-            del package[pid]
-            
-        else: print(-1)
+        popped = -1
+        while len(heap):
+            tmp = heapq.heappop(heap)
+            if tmp.revCost < 0: continue
+            if not package.get(tmp.id): continue
+            popped = tmp.id
+            break
+        print(popped)
+        if popped != -1: del package[popped]
         
     elif query[0] == 500:
         newSrc = query[1]
         dist = daijkstra(connection, newSrc)
+        heap = []
+        for pkg_id, (rev, dst) in package.items():
+            revCost = rev - dist[dst]
+            pkg = Package(pkg_id, revCost)
+            heapq.heappush(heap, pkg)
